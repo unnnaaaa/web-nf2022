acad62ac6e442b3d4e56e2a574111bb9d0222a95
unnnaaaa <114554567+unnnaaaa@users.noreply.github.com>
2022-12-07 12:28:54 +0700

    Add files via upload

---
+++ Praktikum 10/login/otentikasi.js
@@ -0,0 +1,15 @@
+function login() {
+    event.preventDefault();
+    
+
+    let uname = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+
+    if(uname == "ahmad2017" & pass == "integrity") {
+        location.replace("sukses.html")
+        alert("Login Berhasil");
+    } else {
+        alert("Login Gagal");
+    }
+
+}
